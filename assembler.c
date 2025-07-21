72227f9078635c2751f0b4b51f7df10e4c5ee0ca
Emanuel7699 <emanuel7699@gmail.com>
2025-07-21 22:00:07 +0300

    21.7.25

--- assembler.c
+++ assembler.c
@@ -12,6 +12,7 @@ int main (int argc, char *argv[]) {
         i++;
         file = file_extension(argv[i], ".as");
         read_file(file);
+        free(file);
     }
     return 0;
 }
@@ -32,44 +33,52 @@ char *file_extension(char *filename, char *ending) {
 
 
 int read_file(char *filename){
-    int line_number=1;
-    FILE *in = NULL;
-    FILE *out = NULL;
-    FILE *temp_macro = NULL;
-    char line[256],line_temp[256];
+    int flag = 1;
+    FILE *in = NULL, *out = NULL, *temp_macro = NULL;
+    char line[80],line_temp[80],macro_line[80];
     char *newfile = file_extension(filename,".am");
+
     in = fopen(filename, "r");
     out = fopen(newfile, "w");
-    temp_macro = fopen("temp_macro.am", "w");
+    temp_macro = fopen("temp_macro.am", "w+");
+
+
     while (fgets(line, sizeof(line), in) != NULL) {
-        if (strstr(line, "mcro") == NULL) {
+        if (strstr(line, "mcro") == NULL) {/*Not macro*/
+            rewind(temp_macro);
+            sprintf(macro_line, "mcro %s", line);
+
             while (fgets(line_temp, sizeof(line_temp), temp_macro) != NULL) {
-                if (strstr(line_temp, line) != NULL) {
-                    fputs(line_temp, out);
+                if (strstr(line_temp, macro_line) != NULL){/*if the line appear in "mcro line"*/
+                    flag = 0;
+                    while (fgets(line_temp, sizeof(line_temp), temp_macro) != NULL) {
+                        if (strstr(line_temp, "mcroend") != NULL) {
+                            break;
+                        }
+                        fputs(line_temp, out);
+                    }
+                    break;
                 }
             }
-            fputs(line, out);
+            if (flag){
+                fputs(line, out);
+            }
+            flag = 1;
         }
-        else {
-            fprintf(temp_macro, "%d %s", line_number, line);
-            while (fgets(line, sizeof(line), in) != NULL)  {
+        else {/*Macro*/
+            fputs(line, temp_macro);
+            while (fgets(line, sizeof(line), in) != NULL) {
+                fputs(line, temp_macro);
                 if (strstr(line, "mcroend") != NULL) {
                     break;
                 }
-                /*if (strstr(line, "mcroend") != NULL) {
-                    fputs(line, temp_macro);
-                    break;
-                }
-                fputs(line, temp_macro);*/
             }
         }
-        if (strstr(line, "...") != NULL) {
-            while (fgets(line, sizeof(line), in) != NULL)
-            fputs(line, out);
-        }
-        line_number++;
     }
+    fclose(temp_macro);
     fclose(in);
     fclose(out);
+    free(newfile);
+    remove("temp_macro.am");
     return 0;
 }
